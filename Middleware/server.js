f2a9da1b1c511f3fd01cd15baddf828e2fd8d582
Biraj-timilsina <birajtimilsina123@gmail.com>
2021-07-14 13:53:48 +0545

    middleware for notification

---
+++ Middleware/server.js
@@ -0,0 +1,17 @@
+// const { Socket } = require("dgram");
+// var express = require("express")
+// var app = express();
+
+// var http = require("http").createserver(app);
+// var io = require("socket.io")(http);
+
+// http.listen(500,function(){
+//     console.log("connected");
+//     io.on ("connection", function (shocket){
+//         console.log ("User" + Socket.id);
+
+//         Socket.on("notification", function(bid){
+//             Socket.broadcast.emit("notification", bid);
+//         });
+//     });
+// });
