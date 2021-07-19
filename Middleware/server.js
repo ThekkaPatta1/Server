9895f10995926bf65168d3867759a9798ac174ed
Biraj-timilsina <birajtimilsina123@gmail.com>
2021-07-19 15:11:59 +0545

    initialized messenger

--- Middleware/server.js
+++ Middleware/server.js
@@ -1,17 +1,17 @@
-// const { Socket } = require("dgram");
-// var express = require("express")
-// var app = express();
+const { Socket } = require("dgram");
+var express = require("express")
+var app = express();
 
-// var http = require("http").createserver(app);
-// var io = require("socket.io")(http);
+var http = require("http").createserver(app);
+var io = require("socket.io")(http);
 
-// http.listen(500,function(){
-//     console.log("connected");
-//     io.on ("connection", function (shocket){
-//         console.log ("User" + Socket.id);
+http.listen(500,function(){
+    console.log("connected");
+    io.on ("connection", function (shocket){
+        console.log ("User" + Socket.id);
 
-//         Socket.on("notification", function(bid){
-//             Socket.broadcast.emit("notification", bid);
-//         });
-//     });
-// });
+        Socket.on("notification", function(bid){
+            Socket.broadcast.emit("notification", bid);
+        });
+    });
+});
---
+++ hooks/localstorage.js
@@ -0,0 +1,12 @@
+import {useEffect, useState} from 'react'
+
+const PREFIX = 'messenger'
+
+export default function localStorage(key, initialvalue)
+{
+    return(
+        <div>
+
+        </div>
+    )
+}
