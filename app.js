68c7f87f3b9b23053d7c74c391c1ba8e4dd0d351
budabishal280@gmail.com <sirajaryal69@gmail.com>
2021-08-17 11:12:46 +0545

    chnages

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser') //core module
 const cors = require('cors');
 
 const connectDB  = require('./database/db');
-const http = require('http')
+// const http = require('http')
 
 const Loginuser_route = require('./routes/Loginuser_route');
 const Loginworker_route = require('./routes/Loginworker_route');
@@ -43,41 +43,41 @@ app.use(FavWorker_route);
 // const server = http.createServer(app);
 // const { io } = require("./socket/config");
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-const socketIo = require("socket.io");
-const io = socketIo(server);
+// const socketIo = require("socket.io");
+// const io = socketIo(server);
 
-let interval;
+// let interval;
 
-io.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
-  });
-});
-io.on("connection", (socket) => {
-    console.log("New client connected");
-    if (interval) {
-      clearInterval(interval);
-    }
-    interval = setInterval(() => getApiAndEmit(socket), 1000);
-    socket.on("disconnect", () => {
-      console.log("Client disconnected");
-      clearInterval(interval);
-    });
-  });
-const getApiAndEmit = socket => {
-const response = new Date();
-    // Emitting a new message. Will be consumed by the client
-    socket.emit("FromAPI", response);
-  };
-io.attach(app);
+// io.on("connection", (socket) => {
+//   console.log("New client connected");
+//   if (interval) {
+//     clearInterval(interval);
+//   }
+//   interval = setInterval(() => getApiAndEmit(socket), 1000);
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected");
+//     clearInterval(interval);
+//   });
+// });
+// io.on("connection", (socket) => {
+//     console.log("New client connected");
+//     if (interval) {
+//       clearInterval(interval);
+//     }
+//     interval = setInterval(() => getApiAndEmit(socket), 1000);
+//     socket.on("disconnect", () => {
+//       console.log("Client disconnected");
+//       clearInterval(interval);
+//     });
+//   });
+// const getApiAndEmit = socket => {
+// const response = new Date();
+//     // Emitting a new message. Will be consumed by the client
+//     socket.emit("FromAPI", response);
+//   };
+// io.attach(app);
 
 app.listen(550);
 // app.listen(process.env.PORT);   
