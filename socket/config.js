1c73a5be9071039f3118752e59c0c56aa82f37c7
Agraj Poudel <agraj.p7@gmail.com>
2021-08-17 11:15:41 +0545

    Ready for development pull

--- socket/config.js
+++ socket/config.js
@@ -1,45 +1,45 @@
-const SocketIO = require('socket.io');
+// const SocketIO = require('socket.io');
 
-const io = SocketIO(
-    options={
-      cors:true,
-    //   origins:[domain+":"+port],
-      origins:"*",
-     }
-  );
+// const io = SocketIO(
+//     options={
+//       cors:true,
+//     //   origins:[domain+":"+port],
+//       origins:"*",
+//      }
+//   );
 
-// const io = new Server();
+// // const io = new Server();
 
-var Socket = {
-    emit: function (event, data) {
-        console.log(event, data);
-        io.sockets.emit(event, data);
-    }
-};
-let interval;
+// var Socket = {
+//     emit: function (event, data) {
+//         console.log(event, data);
+//         io.sockets.emit(event, data);
+//     }
+// };
+// let interval;
 
-io.on("connection", function (socket) {
+// io.on("connection", function (socket) {
     
-    if (interval) {
-        clearInterval(interval);
-    }
+//     if (interval) {
+//         clearInterval(interval);
+//     }
     
-    interval = setInterval(() => timer(socket), 1000);
+//     interval = setInterval(() => timer(socket), 1000);
     
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-        clearInterval(interval);
-    });
+//     socket.on("disconnect", () => {
+//         console.log("Client disconnected");
+//         clearInterval(interval);
+//     });
 
-});
+// });
 
-const timer = socket => {
+// const timer = socket => {
 
-  const response = new Date();
-  socket.emit("FromAPI", response);
+//   const response = new Date();
+//   socket.emit("FromAPI", response);
 
-};
+// };
 
 
-exports.Socket = Socket;
-exports.io = io;
+// exports.Socket = Socket;
+// exports.io = io;
